68a11e99c2170be6cc6826e09b1b8f8ca557a268
BARarch <anthonyquivers@gmail.com>
2021-07-21 19:36:18 +0200

    Code Signal Code stub footer modification for results that are not iterable

--- testEnvironmentFooter_CS_B.py
+++ testEnvironmentFooter_CS_B.py
@@ -1,4 +1,9 @@
-    fptr.write('\n'.join(map(str, result)))
+    try:
+        fptr.write('\n'.join(map(str, iter(result))))
+    
+    except TypeError as te:
+        fptr.write(str(result))
+        
     fptr.write('\n')
 
     fptr.close()
